c8a905094533b6927e3d26546716d4d9ff73cc9a
MHRSRoni <mdmonirhosen.roni04@gmail.com>
2023-11-13 11:59:09 +0600

    [completed] readlist router added in the main router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 const userProfileRouter = require('./module/user/profile/userProfileRouter')
 const commentRouter = require('./module/comment/commentRouter')
+const readlistRouter = require('./module/readlist/readlistRouter')
+
 const router = require('express').Router();
 
 
 
 router.use('/user', userProfileRouter)
 router.use('/comments', commentRouter)
+router.use('/readlist'. readlistRouter)
 
 
 module.exports = router;
