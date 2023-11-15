fdd2e1072f1980e64310171765ccfe632bd508f4
Ali Rafat <coderrafat@gmail.com>
2023-11-15 19:59:51 +0600

    [completed] user profile read and update

--- src/module/user/profile/userProfileRouter.js
+++ src/module/user/profile/userProfileRouter.js
@@ -1,9 +1,17 @@
-const { userRegisterController, userLoginController, userLogoutController } = require('./userProfileController');
+const { isLoggedIn } = require('../../../auth/auth');
+const { userRegisterController, userLoginController, userLogoutController, userProfileController, userProfileUpdateController } = require('./userProfileController');
 
 const router = require('express').Router();
 
-router.post('/registration', userRegisterController)
-router.post('/login', userLoginController)
-router.get('/logOut', userLogoutController)
+router.post('/registration', userRegisterController);
+
+router.post('/login', userLoginController);
+
+router.get('/logOut', userLogoutController);
+
+router.get('/profile', isLoggedIn, userProfileController);
+
+router.post('/profile/update', isLoggedIn, userProfileUpdateController);
+
 
 module.exports = router;
