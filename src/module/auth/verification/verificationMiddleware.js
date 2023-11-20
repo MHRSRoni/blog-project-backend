08d6cb99a41c3efbcf1de7b7980a1e81f659d4c0
MHRSRoni <mdmonirhosen.roni04@gmail.com>
2023-11-20 15:44:56 +0600

    [fixed] checking if user own's the token

--- src/module/auth/verification/verificationMiddleware.js
+++ src/module/auth/verification/verificationMiddleware.js
@@ -3,7 +3,7 @@ const { decodeToken, decrypt } = require("./verificationUtils")
 exports.isVerifiedFor = requireSubject => async (req, res, next) => {
     try {
         const token = req.query?.['access-token']
-        if(!token) return res.status(400).json({success : false, message : 'unauthorized'})
+        if(!token || data?.email != req.query?.email) return res.status(400).json({success : false, message : 'unauthorized'})
         const verifiedData = await decodeToken(token)
         const data = await decrypt(verifiedData)
         if(data?.subject == requireSubject) return next()
