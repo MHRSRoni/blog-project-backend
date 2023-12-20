ae95328a3617df3931d515c5141828d70e0b2861
MHRSRoni <132211823+MHRSRoni@users.noreply.github.com>
2023-12-20 19:10:40 +0600

    Update loginWithGoogle.js

--- src/utils/loginWithGoogle.js
+++ src/utils/loginWithGoogle.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL,
+    callbackURL: process.env.CALLBACK_URL?.toString()?.trim(),
     // passReqToCallback: true,
 },
     function (accessToken, refreshToken, profile, done) {
